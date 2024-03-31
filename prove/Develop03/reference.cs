1c6aec7dd22956ef32c3050db5afa92db61f1291
ayangjerti <ayangjertee.aj@gmail.com>
2024-03-31 06:00:24 +0100

    scripture memorizer commit

---
+++ prove/Develop03/reference.cs
@@ -0,0 +1,35 @@
+// references
+using System.Collections.Generic;
+
+
+public class Reference 
+{
+    private string _book = "";
+    private int _chapter = 0;
+    private int _verse = 0;
+    private int _endVerse = 0;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+     public Reference(string book, int chapter, int verse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse  = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_book} {_chapter}:{_verse}";
+    }
+
+
+
+
+}
---
+++ prove/Develop03/word.cs
@@ -0,0 +1,51 @@
+// Word.cs
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+         _isHidden = false;
+   
+    }
+
+    public void Hide()
+    {
+           _isHidden = true;
+    }
+
+    public void Show()
+    {
+        if (_isHidden == true)
+        {
+            _isHidden = false;
+        }
+    }
+    public bool IsHidden()
+    {
+     if (_text == "_")
+        {
+            _isHidden = true;
+        }
+        else
+        {
+            _isHidden = false;
+        }
+        return _isHidden;   
+    }
+
+    public string GetDisplayWord()
+    {
+        if (_isHidden == true)
+        {
+            return _text = "_";
+        }
+        else
+        {
+            return _text;
+        }
+    }
+
+}
